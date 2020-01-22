fae939ae70007aca0e21d7e483588a9a04fabcb0
AlexanderTarasov11-708 <aleksandrtarasov53@gmail.com>
2020-01-22 18:17:36 +0300

    fix

--- app/src/main/java/com/itis/readmore/MainActivity.kt
+++ app/src/main/java/com/itis/readmore/MainActivity.kt
@@ -11,26 +11,26 @@ import java.io.IOException
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
+import com.folioreader.Config
 import com.folioreader.FolioReader
 import com.folioreader.model.locators.ReadLocator
 import com.folioreader.util.ReadLocatorListener
 import org.readium.r2.shared.Locations
 import com.folioreader.model.locators.ReadLocator.Companion.LOG_TAG
-
-
+import com.folioreader.util.AppUtil
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var folioReader : FolioReader
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val folioReader = FolioReader.get()
-
-        folioReader.openBook("file:///TheSilverChair.epub")
-    }
-
+        folioReader = FolioReader.get()
+        folioReader.openBook("file:///android_asset/TheSilverChair.epub")
+       }
 }
 
 
